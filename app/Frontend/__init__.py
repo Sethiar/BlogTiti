111c74492b32f02221744a04a22b5ca626d60a8a
Lefetey Arnaud <alefetey123@gmail.com>
2024-08-21 22:11:09 +0200

    Routes et __init__ frontend pour vidéos.

--- app/Frontend/__init__.py
+++ app/Frontend/__init__.py
@@ -1,5 +1,5 @@
 """
-Code permettant de définir les routes concernant les fonctions utilisateurs du blog.
+Code permettant de définir l'accès aux routes concernant les fonctions utilisateurs du blog.
 """
 
 from flask import Blueprint
--- app/Frontend/routes.py
+++ app/Frontend/routes.py
@@ -9,11 +9,16 @@ from flask import abort
 from flask_login import current_user
 
 from app.Models.forms import NewSubjectForumForm, CommentSubjectForm, CommentLike, SuppressCommentForm, \
-    SuppressReplySubject
+    SuppressReplySubject, CommentVideoForm, SuppressCommentVideoForm, SuppressReplyVideo
 
 from app.Models.subject_forum import SubjectForum
 from app.Models.comment_subject import CommentSubject
 from app.Models.likes_comment_subject import CommentLikeSubject
+
+from app.Models.videos import Video
+
+from app.Models.comment_video import CommentVideo
+from app.Models.likes_comment_video import CommentLikeVideo
 
 from flask import render_template, request
 
@@ -100,40 +105,60 @@ def forum_subject(subject_id):
 @frontend_bp.route('/acces-videos')
 def show_videos():
     """
+    Affiche toutes les vidéos de la chaîne Tititechnique avec pagination.
 
-    :return:
+    Cette route récupère toutes les vidéos disponibles dans la base de données et les affiche en utilisant la pagination.
+    Le nombre de vidéos par page est défini et la page actuelle est déterminée par les paramètres de la requête.
+
+    :return: Le template HTML 'frontend/videos.html' rendu avec les vidéos paginées, la page actuelle, et le nombre total de pages.
+
+    Description:
+        - Récupère toutes les vidéos de la base de données en utilisant la fonction `get_videos_from_db`.
+        - Calcule l'index des vidéos à afficher en fonction du numéro de la page demandée et du nombre de vidéos par page.
+        - Rend le template 'frontend/videos.html' avec les vidéos de la page actuelle, ainsi que les informations de pagination
+          (numéro de page et nombre total de pages).
     """
-    per_page = 9  # Nombre de vidéos par page
-    page = request.args.get('page', 1, type=int)  # Récupère le numéro de page, par défaut 1
+    # Nombre de vidéos par page.
+    per_page = 9
+    # Récupération du numéro de page, par défaut 1.
+    page = request.args.get('page', 1, type=int)
 
-    # Créer une instance de YouTubeManager ou utiliser une méthode statique/de classe
+    # Création d'une instance de YouTubeManager.
     videos = get_videos_from_db()
 
-    # Calculer l'index des vidéos à afficher
+    # Calcul de l'index des vidéos à afficher.
     start = (page - 1) * per_page
     end = start + per_page
 
     paginated_videos = videos[start:end]
+    # Calcul du nombre total de pages.
+    total_pages = (len(videos) + per_page - 1) // per_page
 
-    total_pages = (len(videos) + per_page - 1) // per_page  # Calculer le nombre total de pages
-
-    return render_template(
-        'frontend/videos.html',
-        videos=paginated_videos,
-        page=page,
-        total_pages=total_pages
-    )
+    return render_template('frontend/videos.html', videos=paginated_videos, page=page,total_pages=total_pages)
 
 
 # Route permettant de récupérer les vidéos populaires.
 @frontend_bp.route('/popular_videos')
 def show_popular_videos():
     """
+    Affiche les vidéos populaires.
 
-    :return:
+    Cette route récupère toutes les vidéos disponibles dans la base de données, puis filtre et affiche celles qui sont
+    considérées comme populaires en utilisant une fonction dédiée.
+
+    :return: Le template HTML 'popular_videos.html' rendu avec la liste des vidéos populaires.
+
+    Description:
+        - Récupère toutes les vidéos de la base de données en utilisant la fonction `get_videos_from_db`.
+        - Filtre ces vidéos pour obtenir uniquement celles considérées comme populaires en utilisant la fonction
+          `popular_videos`.
+        - Rend le template 'popular_videos.html' avec les vidéos populaires récupérées.
     """
+    # Récupération des vidéos de la base de données.
     videos = get_videos_from_db()
+    # Récupération des vidéos populaires avec la fonction popular_videos().
     popular = popular_videos(videos)
+
     return render_template('popular_videos.html', videos=popular)
 
 
@@ -141,27 +166,92 @@ def show_popular_videos():
 @frontend_bp.route('video/archives/<month_year>')
 def show_archived_videos(month_year):
     """
+    Affiche les vidéos archivées pour un mois et une année donnés.
 
-    :param month_year:
-    :return:
+    Cette route récupère les vidéos archivées pour le mois et l'année spécifiés dans l'URL, et les affiche
+
+    :param month_year: Une chaîne de caractères représentant le mois et l'année des vidéos archivées à afficher,
+                       au format 'MM-YYYY' (par exemple, '08-2024').
+
+    :return: Le template HTML 'frontend/archived_videos.html' rendu avec les vidéos archivées correspondant au mois
+             et à l'année spécifiés, ainsi que les informations de pagination.
+
+    Description:
+        - Récupère les vidéos archivées à partir de la base de données en utilisant la fonction `archived_videos`.
+        - Filtre les vidéos pour obtenir celles correspondant au mois et à l'année fournis.
     """
-    from app.utils_videos import archived_videos
-    per_page = 9  # Nombre de vidéos par page
-    page = request.args.get('page', 1, type=int)  # Récupère le numéro de page, par défaut 1
+    # Nombre de vidéos par page
+    per_page = 9
+    # Récupération du numéro de page, par défaut 1
+    page = request.args.get('page', 1, type=int)
 
-    # Utiliser la méthode archived_videos pour récupérer les vidéos archivées
-    videos = get_videos_from_db()  # Récupérer toutes les vidéos pour générer les archives
-    archived = archived_videos(videos)  # Créez le dictionnaire des vidéos archivées
+    # Utiliser la méthode archived_videos pour récupérer les vidéos archivées.
+    # Récupération de toutes les vidéos.
+    videos = get_videos_from_db()
+    # Création du dictionnaire des vidéos archivées.
+    archived = archived_videos(videos)
     archived = archived.get(month_year, [])
 
-    # Calculer l'index des vidéos à afficher
+    # Calcul de l'index des vidéos à afficher.
     start = (page - 1) * per_page
     end = start + per_page
 
     paginated_videos = archived[start:end]
 
-    total_pages = (len(archived) + per_page - 1) // per_page  # Calculer le nombre total de pages
+    # Calcul du nombre total de pages.
+    total_pages = (len(archived) + per_page - 1) // per_page
 
     return render_template('frontend/archived_videos.html', month_year=month_year, archived=archived,
                            videos=paginated_videos, page=page, total_pages=total_pages
                            )
+
+
+# Route permettant de visualiser une vidéo en particulier afin de laisser un commentaire.
+@frontend_bp.route('/affichage-video/<int:video_id>', methods=['GET', 'POST'])
+def display_video(video_id):
+    """
+    Route permettant d'accéder à une vidéo particulière du blog.
+
+    Args:
+        video_id (int): L'identifiant de la vidéo à afficher.
+
+    Returns:
+        Template HTML 'frontend/video.html' avec les détails de la vidéo et ses commentaires associés.
+
+    Raises:
+        404 Error: Si aucune vidéo correspondant à l'ID spécifié n'est trouvée dans la base de données.
+    """
+    # Création de l'instance des formulaires.
+    formcomment = CommentVideoForm()
+    formlikecomment = CommentLike()
+    formsuppress = SuppressCommentVideoForm()
+    formsuppressreply = SuppressReplyVideo()
+
+    # Récupération due la vidéo spécifiée par l'id depuis la base de données.
+    video = Video.query.get_or_404(video_id)
+
+    # Vérification de l'existence due la vidéo.
+    if not video:
+        # Si la vidéo n'existe pas, erreur 404 renvoyée.
+        abort(404)
+
+    # Récupération des commentaires associés à cette vidéo.
+    comment_video = CommentVideo.query.filter_by(video_id=video_id).all()
+
+    # Préparation des données de likes pour chaque commentaire.
+    comment_likes_data = {}
+    for comment in comment_video:
+        like_count = CommentLikeVideo.query.filter_by(comment_id=comment.id).count()
+        liked_user_ids = [like.user_id for like in CommentLikeVideo.query.filter_by(comment_id=comment.id).all()]
+        liked_by_current_user = current_user.id in liked_user_ids
+        comment_likes_data[comment.id] = {
+            "like_count": like_count,
+            "liked_user_ids": liked_user_ids,
+            "liked_by_current_user": liked_by_current_user
+        }
+
+    return render_template("frontend/video.html", video=video, video_id=video_id,
+                           formsuppress=formsuppress, formsuppressreply=formsuppressreply,
+                           comment_video=comment_video, formcomment=formcomment,
+                           formlikecomment=formlikecomment, comment_likes_data=comment_likes_data)
+
